590db91f07cc0c70e64833cdb2ac5d8ef95967ef
M. A. Alim Mukul <mukul.cse2010@gmail.com>
2022-08-29 14:14:07 +0200

    Add "Tab View" as the starting point for the app

---
+++ Recipe_list/Views/RecipeTabView.swift
@@ -0,0 +1,41 @@
+//
+//  RecipeTabView.swift
+//  Recipe_list
+//
+//  Created by M. A. Alim Mukul on 29.08.22.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        
+        
+        TabView {
+            
+            Text("Featured View")
+            
+                .tabItem {
+                VStack {
+                    Image(systemName: "star.fill")
+                    Text("Featured")
+                }
+            }
+            
+            ContentView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "list.bullet")
+                        Text("List")
+                    }
+                }
+            
+        }
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
